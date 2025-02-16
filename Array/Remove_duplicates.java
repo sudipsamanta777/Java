0d95e6c0c2e1091319a79a9cfb1a16a509e11cb7
sudipsamanta777 <imsudip789@gamil.com>
2025-02-16 15:57:04 +0530

    OneProblem add in array

---
+++ Array/Remove_duplicates.java
@@ -0,0 +1,27 @@
+package Java.Array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Remove_duplicates {
+    public  static int removeDuplicates(int[] nums) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i : nums){
+            if(!list.contains(i)){
+                list.add(i);
+            }
+        }
+        for (int j =0; j<list.size(); j++){
+            nums[j] = list.get(j);
+        }
+        return list.size();
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0,0,1,1,1,2,2,3,3,4};
+        System.out.println(removeDuplicates(arr));
+    }
+}
